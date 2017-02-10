7d9c471046b1aa08ac24b9168be7864a4313be95
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-10 16:20:55 -0500

    minor tweaks to css. still need to add footer and hover effects.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 
 function items() {
-    for (i = 0; i < cowboyBebop.results.length; i++) {
-        $(".contentContainer").append(`<div class = "content"> <img src = "${cowboyBebop.results[i].Images[0].url_170x135}" width = "225"/>
+    for (i = 0; i < 16; i++) {
+        $(".contentContainer").append(
+    `<div class = "content"> <a href = "${cowboyBebop.results[i].url}"><img src = "${cowboyBebop.results[i].Images[0].url_170x135}" width = "225"/></a>
     <p class = "title">${cowboyBebop.results[i].title}</p> <p class = "creator"> ${cowboyBebop.results[i].Shop.shop_name}</p>
     <p class = "price">$${cowboyBebop.results[i].price}</p></div>`)
     }
