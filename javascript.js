8c59445abc9eb695fe277de2e9e58347d330358d
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-10 13:41:50 -0500

    Got images to the right size, added titles, usernames, and prices.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
-pictures();
-function pictures() {
-  for (i=0; i<cowboyBebop.results.length; i++){
-    $(".contentContainer").append(`<div class = "content"><img src = "${cowboyBebop.results[i].Images[0].url_170x135}"/></div>`);
-  }
+function items() {
+    for (i = 0; i < cowboyBebop.results.length; i++) {
+        $(".contentContainer").append(`<div class = "content"> <img src = "${cowboyBebop.results[i].Images[0].url_170x135}" width = "225"/>
+    <p class = "title">${cowboyBebop.results[i].title}</p> <p class = "creator"> ${cowboyBebop.results[i].Shop.shop_name}</p>
+    <p class = "price">$${cowboyBebop.results[i].price}</p></div>`)
+    }
 }
 //Put img src inside of div tags inside of template literals.
+
+items();
