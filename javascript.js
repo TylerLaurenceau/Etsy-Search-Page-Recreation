4aebf54297a464959eb8de9e3c7cee6574a47083
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-12 20:29:26 -0500

    tweaked minor bugs on page

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ function items() {
     for (i = 0; i < 16; i++) {
         $(".contentContainer").append(
     `<div class = "content"> <a href = "${cowboyBebop.results[i].url}"><img src = "${cowboyBebop.results[i].Images[0].url_170x135}" width = "225"/></a>
-    <p class = "title">${cowboyBebop.results[i].title}</p> <p class = "creator"> ${cowboyBebop.results[i].Shop.shop_name}</p>
+    <a href = "${cowboyBebop.results[i].url}"><p class = "title">${cowboyBebop.results[i].title}</p></a><p class = "creator"> ${cowboyBebop.results[i].Shop.shop_name}</p>
     <p class = "price">$${cowboyBebop.results[i].price}</p></div>`)
     }
 }
